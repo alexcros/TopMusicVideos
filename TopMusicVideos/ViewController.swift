a27265945ed9846bb103155860c820228adc995c
Alex Cros <info@alexcros.com>
2016-02-21 23:27:01 +0100

    Initial Commit

---
+++ TopMusicVideos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopMusicVideos
+//
+//  Created by Alexandre Cros on 21/02/16.
+//  Copyright © 2016 Alex Cros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
