213073aa7af1a030c675dad84252edfa465ec264
Alex Cros <info@alexcros.com>
2016-03-06 02:03:34 +0100

    MusicVideoTableViewCell: added getVideoImage function to save the images on array with internet connection ON

--- TopMusicVideos/MusicVideoTableViewCell.swift
+++ TopMusicVideos/MusicVideoTableViewCell.swift
@@ -27,7 +27,59 @@ class MusicVideoTableViewCell: UITableViewCell {
     func updateCell(){
         musicTitle.text = video?.name
         rank.text = ("\(video!.rank)")
-        thumbnailImage.image = UIImage(named: "noPhotoAvailable")
+       // thumbnailImage.image = UIImage(named: "noPhotoAvailable")
+        
+        if video!.imageData != nil {
+        
+            print("Get data from array...")
+            thumbnailImage.image = UIImage(data: video!.imageData!)
+        
+        } else {
+            
+            getVideoImage(video!, imageView: thumbnailImage) // IBOutlet
+        }
     }
+    
+    func getVideoImage (video: MusicVideo, imageView: UIImageView) {
+        // background thread
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            
+            let data = NSData(contentsOfURL: NSURL(string: video.imageURL)!)
+        
+            var image : UIImage?
+            
+            if data != nil { // first time put on array
+                    video.imageData = data
+                    image = UIImage(data: data!) // override noPhotoImage
+            }
+            // move back to main queue, URL image overrided
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
